bd23da50b010bde4d06c7ffc3a0b5a84636f8b95
Bala2k4 <140400470+Bala2k4@users.noreply.github.com>
2024-08-21 22:57:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    // Perform custom validation if needed
+
+    // If all validation passes
+    document.getElementById("message").innerText = "Registration complete";
+    return false; // Prevent form submission for demonstration purposes
+}
